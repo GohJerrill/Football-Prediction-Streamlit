d6ac4259a1e3d085efc10463afeb49bdc1ee8bf7
GohJerrill <gohjerrill316@gmail.com>
2026-01-27 04:13:01 +0800

    Initial Commit - 27 Jan 2026

---
+++ Football_Prediction.py
@@ -0,0 +1,16 @@
+import joblib
+import streamlit as st
+import numpy as np
+import pandas as p
+
+## How to run this app:
+## python -m streamlit run Football_Prediction.py ## This one work better
+## streamlit run Football_Prediction.py
+
+'''
+    Commit 1 - Initial Commit - 27 January 2026
+
+'''
+
+
+st.title("WHATS UP MY FIRST APP")
